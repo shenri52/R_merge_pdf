a363e214206d11e8a1f312ee59f6e50ca46fd855
Samuel HENRI <159037914+shenri52@users.noreply.github.com>
2024-02-06 09:03:00 +0100

    Delete script directory

--- script/librairie.R
+++
@@ -1,9 +0,0 @@
-#################### chargement des librairies
-
-library(tidyverse)
-
-# Diviser, combiner et compresser des fichiers PDF 
-library(qpdf)
-
-# Utiliser des boîte de dialogue
-library(svDialogs) 
